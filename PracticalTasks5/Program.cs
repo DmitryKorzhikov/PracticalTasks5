a14b06f979f42af980ce0ceb193d6525e214881e
Коржикoff <off@DESKTOP-DK>
2024-09-23 09:28:54 +0300

    Добавьте файлы проекта.

---
+++ PracticalTasks5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PracticalTasks5
+{
+    internal class Program
+    {
+
+        static String[] SplitText(string text)
+        {
+            string[] arrText = text.Split();
+            return arrText;
+        }
+        static void WriteText(string text)
+        {
+            Console.WriteLine(text);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку которую требуется резделить");
+            String srcText = Console.ReadLine();
+            String[] splittedText;
+            splittedText = SplitText(srcText);
+            foreach (var a in splittedText)
+            {
+                WriteText(a);
+            }
+        }
+    }
+}
---
+++ Task5_2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace PracticalTasks5
+{
+    internal class Program
+    {
+
+        static string ReversWords(string inputPhrase)
+        {
+            string revText = Reverse(inputPhrase);
+            
+            return revText;
+        }
+        static string Reverse(string text)
+        {
+            string[] arrText = text.Split();
+            string reversText = "";
+            for (int i=arrText.Length; i > 0; i--) 
+            {
+                reversText = reversText + arrText[i-1] + " ";
+            }
+             
+            return reversText;
+        }
+        static void WriteText(string text)
+        {
+            Console.WriteLine(text);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку которую требуется резделить");
+            string srcText = Console.ReadLine();
+            string NewReverseLine = ReversWords(srcText);
+            WriteText(NewReverseLine);
+            
+        }
+    }
+}
